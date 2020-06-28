1e70ecf801d8954c6527d4c1ceea93f9ab57cfb7
Dylan Sprague <dylansprague11@hotmail.com>
2020-06-28 14:32:58 -0400

    Added cogs and barebone error handling

--- app.py
+++ app.py
@@ -9,7 +9,23 @@ async def on_ready():
     print('Successfully logged in as {0.user}'.format(bot))
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def load(ctx, extension):
+    """Load a cog"""
+    bot.load_extension(f'cogs.{extension}')
+    await ctx.message.add_reaction('\U00002705')
+    
+
+@bot.command()
+async def unload(ctx, extension):
+    """Unload a cog"""
+    bot.unload_extension(f'cogs.{extension}')
+    await ctx.message.add_reaction('\U00002705')
+
+@bot.command()
+async def reload(ctx, extension):
+    """Reload a cog"""
+    bot.unload_extension(f'cogs.{extension}')
+    bot.load_extension(f'cogs.{extension}')
+    await ctx.message.add_reaction('\U00002705')
 
 bot.run(config.token)
---
+++ cogs/error.py
@@ -0,0 +1,21 @@
+import discord
+import sys
+import traceback
+from discord.ext import commands
+
+class Error(commands.Cog):
+    def __init__k(self, bot):
+        self.bot = bot
+
+    #Basic Error handling
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        #React to a command that had an error with ❌
+        await ctx.message.add_reaction('\U0000274C')
+
+        #Post traceback to console
+        print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
+        traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+
+def setup(bot):
+    bot.add_cog(Error(bot))
