c6cd76f39281bdfba9b9ab6a1ed0f5a2090c4a8a
Dylan Sprague <dylansprague11@hotmail.com>
2020-06-30 14:48:26 -0400

    Added list of default cogs to load on startup

--- app.py
+++ app.py
@@ -3,24 +3,43 @@ from discord.ext import commands
 import config
 
 bot = commands.Bot(command_prefix='!', case_insensitive=True)
+initCogs = ['cogs.error'] #All default cogs that load on startup
 
+#Send confirmation message when logged into discord
 @bot.event
 async def on_ready():
     print('Successfully logged in as {0.user}'.format(bot))
 
+#Load all default cogs
+counter = 0
+for cog in initCogs:
+    try:
+        bot.load_extension(cog)
+        print(f'{cog} loaded successfully')
+    except Exception as e:
+        print(f'{cog} failed to load')
+        counter += 1
+    else:
+        if counter > 0:
+            break
+        else:
+            print('All cogs successfully loaded')
+
+#Manually load a cog
 @bot.command()
 async def load(ctx, extension):
     """Load a cog"""
     bot.load_extension(f'cogs.{extension}')
     await ctx.message.add_reaction('\U00002705')
     
-
+#Manually unload a cog
 @bot.command()
 async def unload(ctx, extension):
     """Unload a cog"""
     bot.unload_extension(f'cogs.{extension}')
     await ctx.message.add_reaction('\U00002705')
 
+#Manually reload a cog
 @bot.command()
 async def reload(ctx, extension):
     """Reload a cog"""
@@ -28,4 +47,5 @@ async def reload(ctx, extension):
     bot.load_extension(f'cogs.{extension}')
     await ctx.message.add_reaction('\U00002705')
 
+#Log bot into discord
 bot.run(config.token)
