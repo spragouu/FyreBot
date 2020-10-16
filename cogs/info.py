25c95d77b97b3349efc219e1132c3ad0343c0fa8
spragouu <dylansprague98@gmail.com>
2020-10-15 22:46:20 -0400

    Updated info - Added portfolio

--- cogs/info.py
+++ cogs/info.py
@@ -10,7 +10,7 @@ class Info(commands.Cog):
         embed = discord.Embed(color=0xEE7700)
         embed.set_author(name='FyreBot', icon_url=self.bot.user.avatar_url)
         embed.add_field(name="Language", value="[Python](https://discord.gg/python) | [Discord.py](https://discord.gg/dpy)", inline=False)
-        embed.add_field(name="Creator", value="spragouu#8197 | [Twitter](https://twitter.com/spragouu) | [Discord](https://discord.gg/uaxgEhu) | [Instagram](https://instagram.com/spragouu)", inline=False)
+        embed.add_field(name="Creator", value="spragouu#8197\n[Twitter](https://twitter.com/spragouu) | [Portfolio](https://portfolio.deadfyre.com/) | [Instagram](https://instagram.com/spragouu)", inline=False)
         
         await ctx.send(embed=embed)
 
