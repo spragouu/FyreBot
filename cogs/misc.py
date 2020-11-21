1544d41c78bdea577701ae517a732e3345865d86
spragouu <dylansprague98@gmail.com>
2020-11-21 03:40:45 -0500

    Added edit message command

--- cogs/misc.py
+++ cogs/misc.py
@@ -10,5 +10,21 @@ class Misc(commands.Cog):
         '''Send a message to a specific channel'''
         await channel.send(' '.join(message))
 
+    @commands.command()
+    async def edit(self, ctx, messageID, newMessage, channel: discord.TextChannel = None):
+        '''Edit a message send by the bot'''
+        if (channel is None):
+            try:
+                message = await ctx.fetch_message(messageID)
+                await message.edit(content = newMessage)
+            except:
+                raise Exception("That message doesn't exist in this channel. Please specify the channel the message is in!")
+        else:
+            try:
+                message = await channel.fetch_message(messageID)
+                await message.edit(content = newMessage)
+            except:
+                raise Exception("That message doesn't exist in the specified channel!")
+
 def setup(bot):
     bot.add_cog(Misc(bot))
